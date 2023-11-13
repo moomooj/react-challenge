f011ea8832f58c5c79b99c153c0730d1a2209c34
moomooj <wndud36395@gmail.com>
2023-11-13 15:47:08 +0900

    for loop를 사용하여 구문 반환하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-const user = {
-  name: "Hedy Lamarr",
-  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
-  imageSize: 90,
-};
+const products = [
+  { title: "Cabbage", id: 1 },
+  { title: "Garlic", id: 2 },
+  { title: "Apple", id: 3 },
+];
+
+const listItems = products.map((product) => (
+  <li key={product.id}>{product.title}</li>
+));
 
 export function App() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={"Photo of " + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-      />
-    </>
-  );
+  return <ul>{listItems}</ul>;
 }
 
 export default App;
