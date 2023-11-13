52899bf82996ddcdeb4e07d4bb3bde828a34c35d
moomooj <wndud36395@gmail.com>
2023-11-13 15:56:30 +0900

    first useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 export function App() {
+  const [count, setCount] = useState(0);
   function handleClick() {
-    alert("You clicked me!");
+    setCount(count + 1);
   }
-  return <button onClick={handleClick}>Click me</button>;
+  return <button onClick={handleClick}>Clicked {count} times</button>;
 }
 
 export default App;
