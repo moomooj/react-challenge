074bf210322b94c04ecdc15100ce6c4e464a1958
moomooj <wndud36395@gmail.com>
2023-11-13 16:10:38 +0900

    first props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
 
-export function App() {
+function MyButton({ count, onClick }: { count: number; onClick: () => void }) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
+}
+
+function App() {
   const [count, setCount] = useState(0);
+
   function handleClick() {
     setCount(count + 1);
   }
-  return <button onClick={handleClick}>Clicked {count} times</button>;
+
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+  );
 }
 
 export default App;
