916706586e55512789b5010b63b340d7e5a5ea54
moomooj <wndud36395@gmail.com>
2023-11-13 15:26:39 +0900

    JSX로 마크업 작성, add style

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ function MyButton() {
 
 export function App() {
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+    <>
+      {/* 빈 태그로 감싸기 */}
+      <div className="avatar">
+        {/* 클래스 추가 */}
+        <h1>Welcome to my app</h1>
+      </div>
+      <div className="avatar"></div>
+      <div className="avatar"></div>
+      <div className="avatar"></div>
+      <div className="avatar"></div>
+    </>
   );
 }
 
