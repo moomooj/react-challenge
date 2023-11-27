4334412db32aca88b5e8d977ef7c91d604dfd652
moomooj <wndud36395@gmail.com>
2023-11-27 23:17:34 +0900

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useEffect } from "react";
-import { createConnection } from "./chat.js";
+import { useMemo, useState } from "react";
 
-export default function ChatRoom() {
-  useEffect(() => {
-    const connection = createConnection();
-    connection.connect();
-    return () => connection.disconnect();
-  }, []);
-  return <h1>Welcome to the chat!</h1>;
+function TodoList({ todos, filter }) {
+  const [newTodo, setNewTodo] = useState("");
+  const visibleTodos = useMemo(() => {
+    // ✅ Does not re-run unless todos or filter change
+    // ✅ todos나 filter가 변하지 않는 한 재실행되지 않음
+    return getFilteredTodos(todos, filter);
+  }, [todos, filter]);
+  // ...
 }
